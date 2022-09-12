f9508b387098e5ddb13eec90860e8a54a44b7916
Ali Ahmad <aliaurangzaib05@gmail.com>
2022-09-12 16:35:09 +0500

    Downloaded the repo and made some changes

---
+++ cypress/e2e/firstTest.cy.js
@@ -0,0 +1,35 @@
+/// <reference types="cypress" />
+
+describe('Backend Testing',()=>{
+
+    beforeEach('Login to App',()=>{
+        cy.loginToApplication()
+    })
+
+    // it('Should log In',()=>{
+    //     cy.log('We logged in Finally!!')
+    // })
+
+    it.only('Verify correct request and response',()=>{
+
+        cy.intercept('POST','https://api.realworld.io/api/articles/').as('PostArticles')
+
+
+        cy.contains(' New Article ').click()
+        cy.get('[formcontrolname="title"]').type('This is the title')
+        cy.get('[formcontrolname="description"]').type('This is the description')
+        cy.get('[formcontrolname="body"]').type('This is the article')
+        cy.get('[placeholder="Enter tags"]').type('ApeX')
+        //cy.get('[type="button"]').click()
+        cy.contains(' Publish Article ').click()
+
+        cy.wait('@PostArticles').then( xhr => {
+            console.log(xhr)
+            expect(xhr.response.statusCode).to.equal(200)
+            expect(xhr.request.body.article.body).to.equal('This is the article')
+            expect(xhr.response.body.article.description).to.equal('This is the description')
+        })
+
+
+    })
+})
